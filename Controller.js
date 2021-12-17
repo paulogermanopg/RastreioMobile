5e65e4e7d5efac1ffa2d25f3e4b61230210acfda
paulogermanopg <paulogermano.123pg@hotmail.com>
2021-12-17 15:08:01 -0200

    Beck-end e frontend do cadastro, criação do QRCODE e compartilhamento do mesmo

--- Controller.js
+++ Controller.js
@@ -2,11 +2,13 @@ const express=require('express')
 const cors=require('cors')
 const bodyParser=require('body-parser')
 const models=require('./models')
+var QRCode = require('qrcode')
 
 const app=express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(express.static('assets'))
 let user = models.User
 let tracking = models.Tracking
 let product = models.Product
@@ -38,6 +40,30 @@ app.post('/verifyPassword', async(req,res) => {
         }
         
     }
+})
+
+app.post('/create', async(req,res) => {
+    let trackingId = ''
+    await tracking.create({
+        userId: req.body.userId,
+        code: req.body.code,
+        local: req.body.local
+    }).then((response) => {
+        trackingId+=response.id
+    })
+
+    await product.create({
+        trackingId: trackingId,
+        name: req.body.product
+    })
+
+    QRCode.toDataURL(req.body.code).then(url => {
+        QRCode.toFile(
+            './assets/img/code.png',
+            req.body.code
+        )
+        res.send(JSON.stringify(url))
+    })
 })
 
 let port = process.env.PORT || 3000
--- src/screens/areaUser/Cadastro.js
+++ src/screens/areaUser/Cadastro.js
@@ -1,21 +1,132 @@
 import React, {useEffect, useState} from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Button } from 'react-native'
 import Header from '../../components/areaUser/Header'
+import config from '../../../config/config.json'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as Sharing from 'expo-sharing'
+import * as FileSystem from 'expo-file-system'
 
 export default function Cadastro(props) {
 
+    const address = config.origin
+    const [code, setCode] = useState(null)
+    const [user, setUser] = useState(null)
+    const [product, setProduct] = useState(null)
+    const [response, setResponse] = useState(null)
+
+    useEffect(() => {
+        getUserId()
+    }, [])
+
+    useEffect(() => {
+        randomCode()
+        setProduct(null)
+    }, [response])
+
+    //capturar o ID do Usiário
+    async function getUserId(){
+        let response = await AsyncStorage.getItem('userData')
+        let json = JSON.parse(response)
+        setUser(json.id)
+    }
+
+    //geração de código random
+    async function randomCode(){
+        let stringAleatoria = '';
+        let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (var i = 0; i < 25; i++) {
+            stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+        }
+        setCode(stringAleatoria)
+    }
+
+    //enviar formulário
+    async function sendForm(){
+        let response = await fetch(`${config.urlRoot}create`,{
+            method: 'POST',
+            body: JSON.stringify({
+                userId: user,
+                code: code,
+                product: product,
+                local: address
+            }),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        let json = await response.json()
+        setResponse(json)
+    }
+
+    //compartilhar QRCode
+    async function shareQRCode() {
+        const image = config.urlRoot+'img/code.png'
+        FileSystem.downloadAsync(
+            image,
+            FileSystem.documentDirectory+'.png'
+        ).then(({uri}) => {
+            Sharing.shareAsync(uri)
+        })
+        
+        await Sharing.shareAsync()
+    }
+
     return (
         <View>
+
             <Header navigation={props.navigation} titulo={'Cadastro'} />
+
+            <View style={styles.container}>
+
+                {response &&
+                    <View>
+
+                        <Image source={{uri:response, height: 180, width: 180}} />
+                        
+                        <Button title='Compartilhar QRCode' onPress={() => shareQRCode()}/>
+
+                    </View>
+                }
+
+                <TextInput
+                    value={product}
+                    placeholder='Nome do produto'
+                    onChangeText={text => setProduct(text)}
+                />
+
+                <TouchableOpacity style={styles.button}
+                    onPress={() => sendForm()}>
+
+                    <Text style={styles.textButton}>
+                        Cadastrar
+                    </Text>
+
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: '#789ac7',
+        width: '85%',
+        height: 45,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10
+    },
+    textButton: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold'
     },
   })
